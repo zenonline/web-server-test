f7b18d25feb65b8df0dcb4c7a22c68d64dfaed83
Zen Ives <dev@zenonline.co.uk>
2015-12-09 12:21:54 +0000

    lecture 87_1_43_p2

---
+++ middle_ware.js
@@ -0,0 +1,15 @@
+
+module.exports = function(){
+	var middle_ware = {
+		require_authentication: function (req, res, next){
+			console.log('private route hit');
+				next();
+		},
+		logger: function (req, res, next){
+			
+			
+			console.log('On '+  new Date().toString() +' via request method:'+req.method+', the page '+req.originalUrl+' was requested');
+			next();
+		}
+	}
+}
